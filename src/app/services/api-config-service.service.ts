e37db51fdf9842a7bc258f1392013ec91b74b8ad
camanrofo <camanrofo@gmail.com>
2025-03-04 09:11:39 -0500

    Environments variables

--- src/app/services/api-config-service.service.ts
+++ src/app/services/api-config-service.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiConfigService {
-  
-  private readonly apiUrl: string = 'http://localhost:8081';
+
+  private readonly apiUrl: string = environment.apiUrl;
 
   getApiUrl(): string {
     return this.apiUrl;
