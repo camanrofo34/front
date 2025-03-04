4a66aac7de9c1de729d284ac46fe5a115d7cc4b8
Mikudayo <146194087+yatagaratsu666@users.noreply.github.com>
2025-03-04 15:05:49 -0500

    hola

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8081'
+    apiUrl: 'http://safiapp.bucaramanga.upb.edu.co:8081'
   };
   
